ce93842b9fe42422a22ec7f527d2abf92f146d90
Enrico <enrico@cryms.com>
2022-03-23 16:19:05 +0100

    Initial commit

---
+++ src/main/java/com/example/allstarauth/AllStarAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.allstarauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllStarAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllStarAuthApplication.class, args);
+	}
+
+}
